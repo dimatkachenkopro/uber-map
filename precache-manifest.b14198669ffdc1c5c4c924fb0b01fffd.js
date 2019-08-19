6342c02bc4bde93f4a954d16876fe55a9419f998
Dima Tkachenko <dimatkachenkopro@gmail.com>
2019-08-19 05:12:10 +0200

    Updates

---
+++ precache-manifest.b14198669ffdc1c5c4c924fb0b01fffd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3652babeddc5dd3dc07f03fea29b8bc",
+    "url": "//dimatkachenkopro.github.io/uber-map/index.html"
+  },
+  {
+    "revision": "32cef9424cd15c6175e4",
+    "url": "//dimatkachenkopro.github.io/uber-map/static/css/main.9b2363ee.chunk.css"
+  },
+  {
+    "revision": "672c53c809605b903c69",
+    "url": "//dimatkachenkopro.github.io/uber-map/static/js/2.d763d317.chunk.js"
+  },
+  {
+    "revision": "32cef9424cd15c6175e4",
+    "url": "//dimatkachenkopro.github.io/uber-map/static/js/main.733e1336.chunk.js"
+  },
+  {
+    "revision": "9d8c7b0b39ac74667650",
+    "url": "//dimatkachenkopro.github.io/uber-map/static/js/runtime~main.0872069c.js"
+  }
+]);
